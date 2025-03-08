8a93b56f7fd2de1d4f1965f78d6a6b04bbbf8c16
Ching Yee Huang <huangchingyee@gmail.com>
2025-03-08 15:50:54 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EventPage from './EventPage'; // Your event listing page
+import EventDetails from './EventDetails.jsx'; // The event details page
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+import FindInterest from './FindInterest.jsx';
+import Dashboard from './Dashboard.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* Event listing page */}
+        <Route path="/signup" element={<Signup />} /> {/* Event listing page */}
+        <Route path="/interest" element={<FindInterest />} /> {/* Event listing page */}
+        <Route path="/events" element={<EventPage />} /> {/* Event listing page */}
+        <Route path="/events/:id" element={<EventDetails />} /> {/* Event details page */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Event details page */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
